6e59b12377d3289ce8d9b768c39534e4121c947b
Kaman <myletter96@mail.ru>
2019-05-04 16:19:06 +0700

    stack  added

--- paranthesis_balanced.py
+++ paranthesis_balanced.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from stack import Stack
+
 def is_paranthesis_balanced(s):
     pass
 
+    open_b = ['(','[','{','<']
+    matches = [('{','}'), ('[',']'), ('(',')'), ('<','>')]
+    stack = Stack(len(s))
+
+    for i in s:
+        if i in open_b:
+            stack.push(i)
+        else:
+            if len(stack) == 0 :
+                return False
+            last_b = stack.pop()
+            if (last_b, i) not in matches:
+                return False
+    return len(stack) == 0
